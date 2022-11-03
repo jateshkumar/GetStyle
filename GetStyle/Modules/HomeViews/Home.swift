f44e73ef55f774ce9e29148df5d6ca294a728488
Jatesh Kumar <jateshkumarmaheshwari@gmail.com>
2022-11-03 19:33:22 +0500

    [Home slider carousel effect]

--- GetStyle/Modules/HomeViews/Home.swift
+++ GetStyle/Modules/HomeViews/Home.swift
@@ -10,13 +10,45 @@ import SwiftUI
 struct Home: View {
     
     @ObservedObject var viewRouter: ViewRouter
-    @State var selectedTab = "house"
+    @State var currentIndex: Int = 1
+    @State var slides: [SliderModel] = []
     
     var body: some View {
         NavigationView {
-            VStack {
-                Slider()
+            VStack(spacing: 15) {
+                Slider(index: $currentIndex, items: slides) { slide in
+                    GeometryReader { proxy in
+                        
+                        let size = proxy.size
+                        
+                        Image(slide.slideImage)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: size.width)
+                            .cornerRadius(12)
+                            .shadow(color: Color(hex: "#47684f") .opacity(0.7), radius: 5, x: 0, y: 10)
+                    }
+                }
+                .frame(height: 200, alignment: .top)
+                .onAppear {
+                    for index in 1...3 {
+                        slides.append(.init(slideImage: "\(index)"))
+                    }
+                }
+                .padding(.vertical, 20)
+                
+                HStack(spacing: 10) {
+                    ForEach(slides.indices, id: \.self) { index in
+                        Circle()
+                            .fill(Color.black.opacity(currentIndex == index ? 1: 0.1))
+                            .frame(width: 10, height: 10)
+                            .scaleEffect(currentIndex == index ? 1.4 : 1)
+                            .animation(.spring(), value: currentIndex == index)
+                    }
+                }
+                
                 Spacer()
+                
             }
             .navigationTitle("Home")
             .navigationBarTitleDisplayMode(.large)
@@ -25,3 +57,14 @@ struct Home: View {
     }
 }
 
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(viewRouter: ViewRouter())
+    }
+}
+
+
+struct SliderModel: Identifiable {
+    var id = UUID().uuidString
+    var slideImage: String
+}
--- GetStyle/Modules/HomeViews/Slider.swift
+++ GetStyle/Modules/HomeViews/Slider.swift
@@ -7,24 +7,73 @@
 
 import SwiftUI
 
-struct Slider: View {
+struct Slider<Content: View, T: Identifiable>: View {
+    var content: (T) -> Content
+    var list: [T]
     
-    private let slides = ["1", "2", "3"]
+    // Properties
+    var spacing: CGFloat
+    var trailingSpace: CGFloat
+    @Binding var index: Int
+    
+    init(
+        spacing: CGFloat = 15,
+        trailingSpace: CGFloat = 100,
+        index: Binding<Int>,
+        items: [T],
+        @ViewBuilder content: @escaping (T) -> Content) {
+            self.list = items
+            self.spacing = spacing
+            self.trailingSpace = trailingSpace
+            self._index = index
+            self.content = content
+    }
+    
+    @GestureState var offset: CGFloat = 0
+    @State var currentIndex: Int = 1
     
     var body: some View {
-        VStack() {
-            TabView {
-                ForEach(slides, id: \.self) {slide in
-                    Image(slide)
-                        .resizable()
-                        .scaledToFill()
+        GeometryReader { proxy in
+            let width = (proxy.size.width - (trailingSpace - spacing))
+            let adjustmentWidth = (trailingSpace / 2) - spacing
+            
+            HStack(spacing: spacing) {
+                ForEach(list) { item in
+                    content(item)
+                        .frame(width: proxy.size.width - trailingSpace)
                 }
             }
-            .tabViewStyle(.page)
-            .cornerRadius(20)
-            .shadow(color: .black, radius: 10)
+            .padding(.horizontal, spacing)
+            .offset(x: (CGFloat(currentIndex) * -width) + adjustmentWidth + offset)
+            .gesture(
+                DragGesture()
+                    .updating($offset, body: { value, out, _ in
+                        out = value.translation.width
+                    })
+                    .onEnded({ value in
+                        // Updating current index...
+                        let offsetX = value.translation.width
+                        let progress = -offsetX / width
+                        let roundIndex = progress.rounded()
+                        currentIndex = max(min(currentIndex + Int(roundIndex), list.count - 1), 0)
+                        currentIndex = index
+                    })
+                    .onChanged({ value in
+                        // Updating current index...
+                        let offsetX = value.translation.width
+                        let progress = -offsetX / width
+                        let roundIndex = progress.rounded()
+                        index = max(min(currentIndex + Int(roundIndex), list.count - 1), 0)
+                    })
+            )
         }
-        .padding(20)
-        .frame(height: 300)
+        .animation(.easeInOut, value: offset == 0)
+    }
+}
+
+
+struct Slider_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(viewRouter: ViewRouter())
     }
 }
